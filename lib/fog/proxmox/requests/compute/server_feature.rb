3e50258c703cc9394c80d94e39c50d7451148073
Andrew Kent <andrew@sprybts.com>
2014-03-13 16:38:25 +1000

    [ci skip] Proxmox interim

--- lib/fog/proxmox/requests/compute/server_feature.rb
+++ lib/fog/proxmox/requests/compute/server_feature.rb
@@ -4,6 +4,9 @@ module Fog
       class Real
 
         def server_feature(options={})
+          %w[ feature ].each{ |a|
+            raise Fog::Compute::Proxmox::BadRequest.new("Required parameter #{a} is missing.") unless options.include?( a )
+          }
           options.merge!( :command => "nodes/#{options['node']}/#{options['type']}/#{options['vmid']}/feature" )
           %w[ node type vmid ].each { |a| options.delete( a ) }
           request(options)
